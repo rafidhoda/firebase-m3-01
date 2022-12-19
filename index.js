e2f6ee9fba98582e341cda18e76704737a671f89
Rafid Hoda <rafidhoda@gmail.com>
2022-12-19 16:28:59 +0100

    02: creating element variables to manipulate DOM using js - solution

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ Challenge:
 1. Add a const variable for the 'input-field' DOM element
 2. Add a const variable for the 'add-button' DOM element
 */
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
