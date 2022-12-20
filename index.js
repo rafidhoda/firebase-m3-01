3c6273235adee06a477f77ce454818d8251ea35f
Rafid Hoda <rafidhoda@gmail.com>
2022-12-20 16:40:09 +0100

    06: removing array part and clearing input field when button pressed

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ const shoppingListItemsInDB = ref(database, "shoppingListItems")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
-let shoppingListItems = []
-
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
-    shoppingListItems.push(inputValue)
     push(shoppingListItemsInDB, inputValue)
 
-    console.log(inputValue)
+    inputFieldEl.value = ""
 })
